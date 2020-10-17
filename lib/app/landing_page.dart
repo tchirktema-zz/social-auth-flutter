cf092917b45ed18eb30ca75ccc5114b2aac6006e
Josué Tchirktema <tchirktemajosue@gmail.com>
2020-10-17 15:22:33 +0000

    initial config

---
+++ lib/app/landing_page.dart
@@ -0,0 +1,39 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:time_tracker_flutter_course/app/home/home_page.dart';
+import 'package:time_tracker_flutter_course/app/home/jobs/jobs_page.dart';
+import 'package:time_tracker_flutter_course/app/sign_in/sign_in_page.dart';
+import 'package:time_tracker_flutter_course/services/auth.dart';
+import 'package:time_tracker_flutter_course/services/database.dart';
+
+class LandingPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final auth = Provider.of<AuthBase>(context, listen: false);
+    return StreamBuilder<User>(
+        stream: auth.onAuthStateChanged,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.active) {
+            User user = snapshot.data;
+            if (user == null) {
+              return SignInPage.create(context);
+            }
+            return Provider<User>.value(
+              value: user,
+              child: Provider<Database>(
+                builder: (_) => FirestoreDatabase(uid: user.uid),
+                child: HomePage(),
+              ),
+            );
+          } else {
+            return Scaffold(
+              body: Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          }
+        });
+  }
+}
---
+++ lib/services/auth.dart
@@ -0,0 +1,149 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_facebook_login/flutter_facebook_login.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:http/http.dart' as http;
+class User {
+  User({
+    @required this.uid,
+    this.photoUrl,
+    this.displayName,
+  });
+  final String uid;
+  final String photoUrl;
+  final String displayName;
+}
+
+abstract class AuthBase {
+  Stream<User> get onAuthStateChanged;
+  Future<User> currentUser();
+  Future<User> signInAnonymously();
+  Future<User> signInWithEmailAndPassword(String email, String password);
+  Future<User> createUserWithEmailAndPassword(String email, String password);
+  Future<User> signInWithGoogle();
+  Future<User> signInWithFacebook();
+  Future<void> signOut();
+}
+
+class Auth implements AuthBase {
+  final _firebaseAuth = FirebaseAuth.instance;
+
+  User _userFromFirebase(FirebaseUser user) {
+    if (user == null) {
+      return null;
+    }
+    return User(
+      uid: user.uid,
+      displayName: user.displayName,
+      photoUrl: user.photoUrl,
+    );
+  }
+
+  @override
+  Stream<User> get onAuthStateChanged {
+    return _firebaseAuth.onAuthStateChanged.map(_userFromFirebase);
+  }
+
+  @override
+  Future<User> currentUser() async {
+    final user = await _firebaseAuth.currentUser();
+    return _userFromFirebase(user);
+  }
+
+  @override
+  Future<User> signInAnonymously() async {
+    final authResult = await _firebaseAuth.signInAnonymously();
+    return _userFromFirebase(authResult.user);
+  }
+
+  @override
+  Future<User> signInWithEmailAndPassword(String email, String password) async {
+    final authResult = await _firebaseAuth.signInWithEmailAndPassword(
+        email: email, password: password);
+    return _userFromFirebase(authResult.user);
+  }
+
+  @override
+  Future<User> createUserWithEmailAndPassword(
+      String email, String password) async {
+    final authResult = await _firebaseAuth.createUserWithEmailAndPassword(
+        email: email, password: password);
+    return _userFromFirebase(authResult.user);
+  }
+
+  @override
+  Future<User> signInWithGoogle() async {
+    final googleSignIn = GoogleSignIn();
+    final googleAccount = await googleSignIn.signIn();
+    if (googleAccount != null) {
+      final googleAuth = await googleAccount.authentication;
+      print(" idToken: ${googleAuth.idToken}");
+      print(" accessToken: ${googleAuth.accessToken}");
+      if (googleAuth.accessToken != null && googleAuth.idToken != null) {
+
+        final authResult = await _firebaseAuth.signInWithCredential(
+          GoogleAuthProvider.getCredential(
+            idToken: googleAuth.idToken,
+            accessToken: googleAuth.accessToken,
+          ),
+        );
+
+        final idToken = await authResult.user.getIdToken();
+        final token = idToken.token;
+        var client = http.Client();
+        try {
+          var uriResponse = await client.post('http://127.0.0.1:3333/verifyid',
+              body: {'idToken': token});
+          print("=========================");
+          print(uriResponse.body);
+          print("=========================");
+        } finally {
+          client.close();
+        }
+
+        return _userFromFirebase(authResult.user);
+      } else {
+        throw PlatformException(
+          code: 'ERROR_MISSING_GOOGLE_AUTH_TOKEN',
+          message: 'Missing Google Auth Token',
+        );
+      }
+    } else {
+      throw PlatformException(
+        code: 'ERROR_ABORTED_BY_USER',
+        message: 'Sign in aborted by user',
+      );
+    }
+  }
+
+  @override
+  Future<User> signInWithFacebook() async {
+    final facebookLogin = FacebookLogin();
+    final result = await facebookLogin.logInWithReadPermissions(
+      ['public_profile'],
+    );
+    if (result.accessToken != null) {
+      final authResult = await _firebaseAuth.signInWithCredential(
+        FacebookAuthProvider.getCredential(
+          accessToken: result.accessToken.token,
+        ),
+      );
+      return _userFromFirebase(authResult.user);
+    } else {
+      throw PlatformException(
+        code: 'ERROR_ABORTED_BY_USER',
+        message: 'Sign in aborted by user',
+      );
+    }
+  }
+
+  @override
+  Future<void> signOut() async {
+    final googleSignIn = GoogleSignIn();
+    await googleSignIn.signOut();
+    final facebookLogin = FacebookLogin();
+    await facebookLogin.logOut();
+    await _firebaseAuth.signOut();
+  }
+}
